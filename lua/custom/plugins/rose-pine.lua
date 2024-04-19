5a415de42702e4782c5052eff2159629c6264bb9
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-04-19 14:00:07 +0200

    config cleanup

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -1,13 +1,14 @@
-return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
-  priority = 1000,
-  config = function()
-    require('rose-pine').setup {
-      disable_background = true,
-      extend_background_behind_borders = false,
-    }
-
-    -- vim.cmd 'colorscheme rose-pine'
-  end,
-}
+-- return {
+--   'rose-pine/neovim',
+--   name = 'rose-pine',
+--   priority = 1000,
+--   config = function()
+--     require('rose-pine').setup {
+--       disable_background = true,
+--       extend_background_behind_borders = false,
+--     }
+--
+--     -- vim.cmd 'colorscheme rose-pine'
+--   end,
+-- }
+return {}
