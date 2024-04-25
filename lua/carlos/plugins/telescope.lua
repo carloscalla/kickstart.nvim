6135434ad0cfe430aa44131c737684fe99ab39b1
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-04-25 22:20:15 +0200

    cleanup

--- lua/carlos/plugins/telescope.lua
+++ lua/carlos/plugins/telescope.lua
@@ -65,7 +65,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>tr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>ls', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_status<CR>', { noremap = true })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { noremap = true })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
