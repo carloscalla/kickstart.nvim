ad3cdeb37b5fecc55e4131d23d4f50ae45360ded
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-05-04 19:59:42 +0200

    adding toggleterm-manager

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -31,4 +31,13 @@ return {
       'nvim-lua/plenary.nvim',
     },
   },
+  {
+    'ryanmsnyder/toggleterm-manager.nvim',
+    dependencies = {
+      'akinsho/nvim-toggleterm.lua',
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim', -- only needed because it's a dependency of telescope
+    },
+    config = true,
+  },
 }
