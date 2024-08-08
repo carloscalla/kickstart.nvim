f0ee4452427656a00ee2d4a90b838270a3a7ef6e
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-08 13:50:00 +0200

    kickstart catch up 84cc12354dbe0ebda180d445f54820def8c4638f

--- lua/carlos/plugins/nvim-lspconfig.lua
+++ lua/carlos/plugins/nvim-lspconfig.lua
@@ -1,4 +1,4 @@
-return { -- LSP Configuration & Plugins
+return { -- Main LSP Configuration
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -9,20 +9,6 @@ return { -- LSP Configuration & Plugins
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    {
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-        library = {
-          -- Load luvit types when the `vim.uv` word is found
-          { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-        },
-      },
-    },
-    { 'Bilal2453/luvit-meta', lazy = true },
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
