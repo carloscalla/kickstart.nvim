c6eaa9d0225591265a73657bf7332e14625fe652
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-27 14:57:14 +0200

    file path improvements

--- lua/carlos/usercmds.lua
+++ lua/carlos/usercmds.lua
@@ -17,10 +17,13 @@ end, {
 })
 
 vim.api.nvim_create_user_command('FullFilePath', function()
-  vim.fn.setreg('+', vim.fn.expand '%:p')
+  local fullFilePath = vim.fn.expand '%:p'
+  vim.fn.setreg('+', fullFilePath)
+  print(fullFilePath)
 end, { desc = 'Copy full path of current buffer to clipboard' })
 
 vim.api.nvim_create_user_command('FilePath', function()
-  -- copy relative path of current buffer to clipboard
-  vim.fn.setreg('+', vim.fn.expand '%:~:.')
+  local filePath = vim.fn.expand '%:p:~:.'
+  vim.fn.setreg('+', filePath)
+  print(filePath)
 end, { desc = 'Copy relative path of current buffer to clipboard' })
