bee67a70b977d46fdbe71bd03782c0ddcf61a19b
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-29 13:38:48 +0200

    mini surround & flash improvements

--- lua/carlos/plugins/mini.lua
+++ lua/carlos/plugins/mini.lua
@@ -14,7 +14,19 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      --[[
+      mappings = {
+        add = 'gsa', -- Add surrounding in Normal and Visual modes
+        delete = 'gsd', -- Delete surrounding
+        find = 'gsf', -- Find surrounding (to the right)
+        find_left = 'gsF', -- Find surrounding (to the left)
+        highlight = 'gsh', -- Highlight surrounding
+        replace = 'gsr', -- Replace surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
+      },
+      ]]
+    }
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
