d64dd1e653a66effbd379e00b6699596078560da
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-31 01:30:55 +0200

    fix octo

--- lua/carlos/plugins/octo.lua
+++ lua/carlos/plugins/octo.lua
@@ -10,19 +10,15 @@ return {
     require('octo').setup {
       -- FzF-lua throwing errors, specially with `search` command
       -- picker = 'fzf-lua',
-      {
-        suppress_missing_scope = {
-          projects_v2 = true,
-        },
-      },
-
-      vim.api.nvim_create_user_command('GhPrTeam', function()
-        vim.cmd [[ Octo pr search is:open review-requested:@me ]]
-      end, { desc = 'Github PRs assigned to me or my team' }),
-
-      vim.api.nvim_create_user_command('GhPrMe', function()
-        vim.cmd [[ Octo pr search is:open user-review-requested:@me ]]
-      end, { desc = 'Github PRs assigned to me' }),
+      default_to_projects_v2 = true,
     }
+
+    vim.api.nvim_create_user_command('GhPrTeam', function()
+      vim.cmd [[ Octo pr search is:open review-requested:@me ]]
+    end, { desc = 'Github PRs assigned to me or my team' })
+
+    vim.api.nvim_create_user_command('GhPrMe', function()
+      vim.cmd [[ Octo pr search is:open user-review-requested:@me ]]
+    end, { desc = 'Github PRs assigned to me' })
   end,
 }
