1ebe7d757c13eb6c1134705ab2b38e37d397e8f1
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-31 02:04:05 +0200

    octo lazy load

--- lua/carlos/plugins/octo.lua
+++ lua/carlos/plugins/octo.lua
@@ -1,5 +1,7 @@
 return {
   'pwntester/octo.nvim',
+  cmd = 'Octo',
+  event = { { event = 'BufReadCmd', pattern = 'octo://*' } },
   dependencies = {
     'nvim-lua/plenary.nvim',
     -- 'nvim-telescope/telescope.nvim',
@@ -10,9 +12,11 @@ return {
     require('octo').setup {
       -- FzF-lua throwing errors, specially with `search` command
       -- picker = 'fzf-lua',
+      picker = 'telescope',
       default_to_projects_v2 = true,
     }
-
+  end,
+  init = function()
     vim.api.nvim_create_user_command('GhPrTeam', function()
       vim.cmd [[ Octo pr search is:open review-requested:@me ]]
     end, { desc = 'Github PRs assigned to me or my team' })
