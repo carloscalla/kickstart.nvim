33cc1eb30601e8f94e537691b63d306b99cd04ca
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-08-31 23:44:18 +0200

    move autocmds to plugin file persistence

--- lua/carlos/plugins/persistence.lua
+++ lua/carlos/plugins/persistence.lua
@@ -7,4 +7,23 @@ return {
     pre_save = nil, -- a function to call before saving the session
     save_empty = false, -- don't save if there are no open file buffers
   },
+  init = function()
+    vim.api.nvim_create_user_command('PersistenceRestoreCwd', function()
+      require('persistence').load()
+    end, {
+      desc = 'Restore the session for the current directory',
+    })
+
+    vim.api.nvim_create_user_command('PersistenceRestoreLast', function()
+      require('persistence').load { last = true }
+    end, {
+      desc = 'Restore the last session',
+    })
+
+    vim.api.nvim_create_user_command('PersistenceStop', function()
+      require('persistence').stop()
+    end, {
+      desc = "Stop Persistence => session won't be saved on exit",
+    })
+  end,
 }
--- lua/carlos/usercmds.lua
+++ lua/carlos/usercmds.lua
@@ -1,21 +1,3 @@
-vim.api.nvim_create_user_command('PersistenceRestoreCwd', function()
-  require('persistence').load()
-end, {
-  desc = 'Restore the session for the current directory',
-})
-
-vim.api.nvim_create_user_command('PersistenceRestoreLast', function()
-  require('persistence').load { last = true }
-end, {
-  desc = 'Restore the last session',
-})
-
-vim.api.nvim_create_user_command('PersistenceStop', function()
-  require('persistence').stop()
-end, {
-  desc = "Stop Persistence => session won't be saved on exit",
-})
-
 vim.api.nvim_create_user_command('FullFilePath', function()
   local fullFilePath = vim.fn.expand '%:p'
   vim.fn.setreg('+', fullFilePath)
