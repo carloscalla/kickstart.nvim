07d2e7defdee0a9f7f539d7ce0610b68bea3b7ba
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-03 13:17:28 +0200

    gh-co lazy by cmd

--- lua/carlos/plugins/gh-co.lua
+++ lua/carlos/plugins/gh-co.lua
@@ -1,3 +1,11 @@
 return {
   'https://github.com/comatory/gh-co.nvim',
+  cmd = {
+    'GhCoHealthcheck',
+    'GhCoStatus',
+    'GhCoShowFile',
+    'GhCoWho',
+    'GhCoWhos',
+    'GhCoGitWho',
+  },
 }
