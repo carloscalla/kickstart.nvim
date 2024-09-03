2fa3b6918d7244747d5f31639c1e36f4295f48cb
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-03 23:43:27 +0200

    toggleterm lazy improvements

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -66,15 +66,6 @@ return {
       'nvim-lua/plenary.nvim',
     },
   },
-  {
-    'ryanmsnyder/toggleterm-manager.nvim',
-    dependencies = {
-      'akinsho/nvim-toggleterm.lua',
-      'nvim-telescope/telescope.nvim',
-      'nvim-lua/plenary.nvim', -- only needed because it's a dependency of telescope
-    },
-    config = true,
-  },
 
   -- Git related plugins
   {
--- lua/carlos/plugins/toggleterm.lua
+++ lua/carlos/plugins/toggleterm.lua
@@ -1,13 +1,33 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  config = function()
-    require('toggleterm').setup {
-      size = 30,
-      direction = 'horizontal',
-    }
-  end,
-  keys = {
-    { '<leader>tt', '<Cmd>ToggleTerm<CR>' },
+  {
+    'akinsho/toggleterm.nvim',
+    dependencies = {
+      'ryanmsnyder/toggleterm-manager.nvim',
+      dependencies = {
+        'nvim-telescope/telescope.nvim',
+        'nvim-lua/plenary.nvim', -- only needed because it's a dependency of telescope
+      },
+      config = true,
+    },
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        size = 30,
+        direction = 'horizontal',
+      }
+    end,
+    keys = {
+      { '<leader>tt', '<Cmd>ToggleTerm<CR>' },
+    },
   },
+  -- Original toggleterm-manager config from docs
+  -- {
+  --   'ryanmsnyder/toggleterm-manager.nvim',
+  --   lazy = true,
+  --   dependencies = {
+  --     'akinsho/toggleterm.nvim',
+  --     'nvim-telescope/telescope.nvim',
+  --     'nvim-lua/plenary.nvim', -- only needed because it's a dependency of telescope
+  --   },
+  -- },
 }
