04440459e0335d52da22007d3e5cb662037d2025
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-05 11:07:39 +0200

    rename tsserver to ts_ls

--- lua/carlos/plugins/nvim-lspconfig.lua
+++ lua/carlos/plugins/nvim-lspconfig.lua
@@ -156,8 +156,8 @@ return { -- Main LSP Configuration
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {
+      -- But for many setups, the LSP (`ts_ls`) will work just fine
+      ts_ls = {
         -- Default
         -- root_dir = root_pattern('tsconfig.json', 'jsconfig.json', 'package.json', '.git'),
 
@@ -260,7 +260,7 @@ return { -- Main LSP Configuration
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          -- certain features of an LSP (for example, turning off formatting for ts_ls)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
