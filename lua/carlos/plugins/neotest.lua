5bff2c32e88dde2395097354e036e4462de8c848
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-16 13:58:51 -0500

    enabling neotest and fixing setup

--- lua/carlos/plugins/neotest.lua
+++ lua/carlos/plugins/neotest.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-neotest/neotest',
-  enabled = false,
   dependencies = {
     'nvim-neotest/nvim-nio',
     'nvim-lua/plenary.nvim',
@@ -12,7 +11,12 @@ return {
     require('neotest').setup {
       adapters = {
         require 'neotest-jest' {
-          jestCommand = 'node_modules/.bin/jest',
+          jestCommand = 'npm test --',
+          jestConfigFile = 'custom.jest.config.ts',
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
         },
       },
     }
