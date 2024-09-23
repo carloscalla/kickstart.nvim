10a2031b74a8cbafc8a9d5623d3b34a46451bf98
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-23 12:30:15 +0200

    fix wrong description

--- lua/carlos/plugins/kulala.lua
+++ lua/carlos/plugins/kulala.lua
@@ -15,7 +15,7 @@ return {
       { '<leader>Rt', "<cmd>lua require('kulala').toggle_view()<cr>", desc = 'Toggle headers/body' },
       { '<leader>Rp', "<cmd>lua require('kulala').jump_prev()<cr>", desc = 'Jump to previous request' },
       { '<leader>Rn', "<cmd>lua require('kulala').jump_next()<cr>", desc = 'Jump to next request' },
-      { '<leader>Ri', "<cmd>lua require('kulala').inspect()<cr>", desc = 'Jump to next request' },
+      { '<leader>Ri', "<cmd>lua require('kulala').inspect()<cr>", desc = 'Inspect request' },
     },
     opts = {},
   },
