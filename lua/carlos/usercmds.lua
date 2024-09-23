852777115c8488e63bd92035c3bf29b9cedfde06
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2024-09-23 13:02:49 +0200

    adds command to copy file name to clipboard

--- lua/carlos/usercmds.lua
+++ lua/carlos/usercmds.lua
@@ -9,3 +9,9 @@ vim.api.nvim_create_user_command('FilePath', function()
   vim.fn.setreg('+', filePath)
   print(filePath)
 end, { desc = 'Copy relative path of current buffer to clipboard' })
+
+vim.api.nvim_create_user_command('FileName', function()
+  local fileName = vim.fn.expand '%:t'
+  vim.fn.setreg('+', fileName)
+  print(fileName)
+end, { desc = 'Copy file name of current buffer to clipboard' })
