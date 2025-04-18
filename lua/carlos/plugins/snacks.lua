58961715e9b28f479c1f62ea7179d56de0618124
Carlos Calla <calla.alarcon@gmail.com>
2025-04-19 01:23:36 +0200

    trying out snacks explorer

--- lua/carlos/plugins/snacks.lua
+++ lua/carlos/plugins/snacks.lua
@@ -29,7 +29,10 @@ return {
     },
     -- bigfile = { enabled = true },
     -- dashboard = { enabled = true },
-    -- explorer = { enabled = true },
+    explorer = {
+      enabled = true,
+      replace_netrw = false,
+    },
     -- indent = { enabled = true },
     -- input = { enabled = true },
     picker = {
@@ -42,6 +45,43 @@ return {
             ['<c-u>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
             ['<c-j>'] = { 'list_scroll_down', mode = { 'i', 'n' } },
             ['<c-k>'] = { 'list_scroll_up', mode = { 'i', 'n' } },
+          },
+        },
+      },
+      sources = {
+        explorer = {
+          auto_close = true,
+          win = {
+            input = {
+              keys = {
+                ['<Esc>'] = 'cancel',
+              },
+            },
+          },
+          layout = {
+            layout = {
+              box = 'horizontal',
+              width = 0.5,
+              min_width = 60,
+              height = 0.8,
+              {
+                box = 'vertical',
+                border = 'rounded',
+                title = '{title} {live} {flags}',
+                {
+                  win = 'input',
+                  height = 1,
+                  border = 'bottom',
+                },
+                { win = 'list', border = 'none' },
+              },
+              {
+                win = 'preview',
+                title = '{preview}',
+                border = 'rounded',
+                width = 0.5,
+              },
+            },
           },
         },
       },
@@ -60,6 +100,20 @@ return {
         Snacks.picker.files()
       end,
       desc = 'Snacks files',
+    },
+    {
+      '<leader>se',
+      function()
+        Snacks.explorer.open { follow_file = false }
+      end,
+      desc = 'Snacks explorer',
+    },
+    {
+      '<leader>sr',
+      function()
+        Snacks.explorer.reveal()
+      end,
+      desc = 'Snacks explorer',
     },
   },
 }
