d9eb5da7dcb1428654b14910094eda227a5ce3f6
Carlos Calla <calla.alarcon@gmail.com>
2025-04-22 00:16:01 +0200

    Update lsp mappings with neovim 0.11 defaults

--- lua/carlos/plugins/nvim-lspconfig.lua
+++ lua/carlos/plugins/nvim-lspconfig.lua
@@ -46,8 +46,8 @@ return { -- Main LSP Configuration
         end, '[G]oto [D]efinition')
 
         -- Find references for the word under your cursor.
-        map('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gr', function()
+        -- map('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('grr', function()
           require('fzf-lua').lsp_references {
             winopts = {
               width = 0.7,
@@ -61,13 +61,13 @@ return { -- Main LSP Configuration
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>gI', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
+        -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('gri', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map('grt', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
@@ -79,11 +79,11 @@ return { -- Main LSP Configuration
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+        -- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
@@ -93,7 +93,7 @@ return { -- Main LSP Configuration
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
