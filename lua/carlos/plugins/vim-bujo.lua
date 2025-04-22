961a5a8c7f0693e2c634a911e31d42a906671678
Carlos Calla <calla.alarcon@gmail.com>
2025-04-22 18:09:05 +0200

    Fix bujo

--- lua/carlos/plugins/vim-bujo.lua
+++ lua/carlos/plugins/vim-bujo.lua
@@ -1,13 +1,6 @@
 return {
   'vuciv/vim-bujo',
-  cmd = { 'TodoApp', 'TodoAppGlobal' },
   init = function()
-    vim.api.nvim_create_user_command('TodoApp', function()
-      vim.cmd [[ botright split Todo]]
-    end, { desc = 'Open Bujo' })
-
-    vim.api.nvim_create_user_command('TodoAppGlobal', function()
-      vim.cmd [[ botright split Todo g]]
-    end, { desc = 'Open Bujo' })
+    vim.cmd [[ let g:bujo#todo_file_path = $HOME . "/.cache/bujo" ]]
   end,
 }
