291c8c1a9fea1827951e9d04167c007f8874091c
Carlos Calla <calla.alarcon@gmail.com>
2025-04-22 18:30:54 +0200

    Lazygit cleanup

--- lua/carlos/plugins/lazygit.lua
+++ lua/carlos/plugins/lazygit.lua
@@ -1,4 +1,3 @@
--- nvim v0.8.0
 return {
   'kdheepak/lazygit.nvim',
   cmd = {
@@ -8,12 +7,9 @@ return {
     'LazyGitFilter',
     'LazyGitFilterCurrentFile',
   },
-  -- optional for floating window border decoration
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
