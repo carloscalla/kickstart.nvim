cbb9aa2e01f4b0fa480832bdaa6dcf186da694fd
Carlos Calla <calla.alarcon@gmail.com>
2025-04-24 14:46:33 +0200

    Update fzf-lua keymap

--- lua/carlos/plugins/fzf-lua.lua
+++ lua/carlos/plugins/fzf-lua.lua
@@ -6,42 +6,70 @@ return {
   },
   config = function()
     local actions = require 'fzf-lua.actions'
-    -- Force image module to load
-    require 'snacks.image'
     require('fzf-lua').setup {
-      -- 'telescope',
-      -- fzf_opts = { ['--layout'] = 'default' },
       keymap = {
-        fzf = {
-          true,
-          ['ctrl-j'] = 'half-page-down',
-          ['ctrl-k'] = 'half-page-up',
-          ['ctrl-d'] = 'preview-page-down',
-          ['ctrl-u'] = 'preview-page-up',
-          ['ctrl-q'] = 'select-all+accept',
-        },
         builtin = {
-          true,
+          -- from detaults
+          ['<F1>'] = 'toggle-help',
+          ['<F2>'] = 'toggle-fullscreen',
+          -- Only valid with the 'builtin' previewer
+          ['<F3>'] = 'toggle-preview-wrap',
+          ['<F4>'] = 'toggle-preview',
+          ['<F5>'] = 'toggle-preview-ccw',
+          ['<F6>'] = 'toggle-preview-cw',
+          ['<F7>'] = 'toggle-preview-ts-ctx',
+          ['<F8>'] = 'preview-ts-ctx-dec',
+          ['<F9>'] = 'preview-ts-ctx-inc',
+          ['<S-Left>'] = 'preview-reset',
+          ['<S-down>'] = 'preview-down',
+          ['<S-up>'] = 'preview-up',
+
+          -- custom
           ['<C-o>'] = 'toggle-preview',
           ['<C-/>'] = 'toggle-help',
           ['<C-d>'] = 'preview-page-down',
           ['<C-u>'] = 'preview-page-up',
         },
+        fzf = {
+          -- from defaults
+          ['ctrl-z'] = 'abort',
+          ['ctrl-a'] = 'beginning-of-line',
+          ['ctrl-e'] = 'end-of-line',
+          ['alt-a'] = 'toggle-all',
+          ['alt-g'] = 'first',
+          ['alt-G'] = 'last',
+          -- Only valid with fzf previewers (bat/cat/git/etc)
+          ['f3'] = 'toggle-preview-wrap',
+          ['f4'] = 'toggle-preview',
+          ['shift-down'] = 'preview-down',
+          ['shift-up'] = 'preview-up',
+
+          -- custom
+          ['ctrl-j'] = 'half-page-down',
+          ['ctrl-k'] = 'half-page-up',
+          ['ctrl-d'] = 'preview-page-down',
+          ['ctrl-u'] = 'preview-page-up',
+          ['ctrl-q'] = 'select-all+accept',
+          ['ctrl-l'] = 'unix-line-discard',
+        },
       },
       actions = {
         files = {
-          true,
+          -- from defaults
           ['enter'] = actions.file_edit_or_qf,
-          ['alt-q'] = actions.file_sel_to_qf,
-          ['alt-l'] = actions.file_sel_to_ll,
           ['ctrl-s'] = actions.file_split,
           ['ctrl-v'] = actions.file_vsplit,
+          ['ctrl-t'] = actions.file_tabedit,
+          ['alt-q'] = actions.file_sel_to_qf,
+          ['alt-f'] = { fn = actions.toggle_follow, reuse = true, header = false },
+
+          -- custom
+          ['alt-l'] = actions.file_sel_to_ll,
           ['alt-h'] = actions.toggle_hidden,
           ['alt-i'] = actions.toggle_ignore,
           -- ctrl-i can't be used because it conflicts with TAB
           -- For more information see:
           -- https://unix.stackexchange.com/questions/563469/conflict-ctrl-i-with-tab-in-normal-mode
-          -- ['ctrl-i'] = actions.toggle_ignore,
         },
       },
       winopts = {
