0f268ad33bc72fba137eda33d38f7f0c3ae2aa46
Carlos Calla <calla.alarcon@gmail.com>
2025-04-26 01:01:46 +0200

    Explictly enable sign in lightbulb

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -89,10 +89,10 @@ return {
   },
   {
     'kosayoda/nvim-lightbulb',
-    enabled = false,
     config = function()
       require('nvim-lightbulb').setup {
         autocmd = { enabled = true },
+        sign = { enabled = true },
       }
     end,
   },
