24cb0c2a43a1258463bcbe4d71035ac90d26fd88
Carlos Calla <calla.alarcon@gmail.com>
2025-04-26 00:43:34 +0200

    Adding lightbulb

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -87,4 +87,13 @@ return {
       },
     },
   },
+  {
+    'kosayoda/nvim-lightbulb',
+    enabled = false,
+    config = function()
+      require('nvim-lightbulb').setup {
+        autocmd = { enabled = true },
+      }
+    end,
+  },
 }
