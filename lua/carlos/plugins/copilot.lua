3a48392a4fcc3673f13b71317d64b2fce091c9c9
Carlos Calla <calla.alarcon@gmail.com>
2025-04-28 15:48:29 +0200

    Update copilot mapping, follow docs suggestions

--- lua/carlos/plugins/copilot.lua
+++ lua/carlos/plugins/copilot.lua
@@ -1,9 +1,13 @@
 return {
   'github/copilot.vim',
   enabled = false,
-  config = function()
-    vim.keymap.set('i', '<C-i>', 'copilot#Accept()', { noremap = true, expr = true, silent = true, replace_keycodes = false })
+  init = function()
     vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
+  end,
+  config = function()
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
   end,
 }
