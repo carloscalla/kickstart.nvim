3799244182e606bf15109bb03c97bc7f613a78e7
Carlos Calla <calla.alarcon@gmail.com>
2025-04-29 00:51:29 +0200

    Removing vim-grepper because it dies on large codebases

--- lua/carlos/plugins/grepper.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'https://github.com/mhinz/vim-grepper',
-  cmd = { 'Grepper', 'GrepperGit', 'GrepperRg', 'GrepperGrep' },
-  config = function()
-    vim.cmd [[ let g:grepper.rg.grepprg .= ' --smart-case' ]]
-  end,
-}
