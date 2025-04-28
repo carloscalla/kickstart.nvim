2396b2ce3f4d5a8d0d2c6bc76cb044bf99bcedf7
Carlos Calla <calla.alarcon@gmail.com>
2025-04-29 00:12:13 +0200

    Enabling vim-grepper with lazy loading

--- lua/carlos/plugins/grepper.lua
+++ lua/carlos/plugins/grepper.lua
@@ -1,6 +1,6 @@
 return {
   'https://github.com/mhinz/vim-grepper',
-  enabled = false,
+  cmd = { 'Grepper', 'GrepperGit', 'GrepperRg', 'GrepperGrep' },
   config = function()
     vim.cmd [[ let g:grepper.rg.grepprg .= ' --smart-case' ]]
   end,
