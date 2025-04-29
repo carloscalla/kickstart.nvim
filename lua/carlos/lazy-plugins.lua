3cda1bab08271381132be9ad98e7912e0c3bd38a
Carlos Calla <calla.alarcon@gmail.com>
2025-04-29 23:38:42 +0200

    Cleanup

--- lua/carlos/lazy-plugins.lua
+++ lua/carlos/lazy-plugins.lua
@@ -10,10 +10,10 @@ require('lazy').setup({
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/carlos/plugins/*.lua`
   --    This is the easiest way to modularize your config.
-  --
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 
   { import = 'carlos.plugins' },
+
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
