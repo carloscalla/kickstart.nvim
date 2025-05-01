47e307ff177616436824f4ce955598953aa64026
Carlos Calla <calla.alarcon@gmail.com>
2025-05-01 02:31:35 +0200

    Refactor treesj setup

--- lua/carlos/plugins/treesj.lua
+++ lua/carlos/plugins/treesj.lua
@@ -1,17 +1,21 @@
 return {
   'Wansmer/treesj',
-  keys = { '<leader>m' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup {
-      ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
-      use_default_keymaps = false,
-    }
-    -- For default preset
-    vim.keymap.set('n', '<leader>m', require('treesj').toggle)
-    -- For extending default preset with `recursive = true`
-    vim.keymap.set('n', '<leader>M', function()
-      require('treesj').toggle { split = { recursive = true } }
-    end)
-  end,
+  opts = {
+    use_default_keymaps = false,
+  },
+  keys = {
+    {
+      '<leader>m',
+      function()
+        require('treesj').toggle()
+      end,
+    },
+    {
+      '<leader>M',
+      function()
+        require('treesj').toggle { split = { recursive = true } }
+      end,
+    },
+  },
 }
