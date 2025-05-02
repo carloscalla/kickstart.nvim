62cd7523c83736257bd244a156709509c76885b0
Carlos Calla <calla.alarcon@gmail.com>
2025-05-02 11:36:02 +0200

    Update flash config to resemble / ? in operation pending mode "o"

--- lua/carlos/plugins/flash.lua
+++ lua/carlos/plugins/flash.lua
@@ -12,13 +12,47 @@ return {
       },
     },
   },
-  -- stylua: ignore
   keys = {
+    -- stylua: ignore start
     { "s"    , mode = { "n", "x" },      function() require("flash").jump() end,                          desc = "Flash" },
-    { "s"    , mode = { "o" },           function() require("flash").jump { jump = { offset = -1 } } end, desc = "Flash" },
     { "<C-,>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,         desc = "Flash Treesitter" },
     { "r"    , mode = { "o" },           function() require("flash").remote() end,                 desc = "Remote Flash" },
     { "R"    , mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,          desc = "Toggle Flash Search" },
+    -- stylua: ignore end
+    {
+      's',
+      mode = { 'o' },
+      function()
+        require('flash').jump {
+          search = {
+            forward = true,
+            wrap = false,
+            multi_window = false,
+          },
+          jump = {
+            offset = -1,
+          },
+        }
+      end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'o' },
+      function()
+        require('flash').jump {
+          search = {
+            forward = false,
+            wrap = false,
+            multi_window = false,
+          },
+          jump = {
+            inclusive = false,
+          },
+        }
+      end,
+      desc = 'Flash',
+    },
   },
 }
