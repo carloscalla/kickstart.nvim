7e08b2b823bdc7771ac9022a572b273b55f59c81
Carlos Calla <calla.alarcon@gmail.com>
2025-05-12 14:43:26 +0200

    fix: rename vim.highlight.on_yank to vim.hl.on_yank

--- lua/carlos/autocmds.lua
+++ lua/carlos/autocmds.lua
@@ -3,13 +3,13 @@
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 --[[ Commenting this out in favor of Yanky
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 ]]
