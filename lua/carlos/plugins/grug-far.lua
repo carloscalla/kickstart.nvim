f9c87021bf240f097cb38b1e56b21d9b9adf3697
Carlos Calla <calla.alarcon@gmail.com>
2025-05-27 13:30:32 +0200

    keymaps for grug-far

--- lua/carlos/plugins/grug-far.lua
+++ lua/carlos/plugins/grug-far.lua
@@ -8,4 +8,22 @@ return {
       -- ... engine = 'ripgrep' is default, but 'astgrep' can be specified...
     }
   end,
+  keys = {
+    {
+      '<leader>gf',
+      function()
+        require('grug-far').open()
+      end,
+      mode = { 'n' },
+      desc = 'Grug Far',
+    },
+    {
+      '<leader>gf',
+      function()
+        require('grug-far').with_visual_selection()
+      end,
+      mode = { 'v' },
+      desc = 'Grug Far selection',
+    },
+  },
 }
