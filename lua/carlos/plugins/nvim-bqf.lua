e98829d5d78161c51749eecb5069ee8f49f7bedd
Carlos Calla <calla.alarcon@gmail.com>
2025-05-30 11:38:53 +0200

    fzf: comment out install script and add notes

--- lua/carlos/plugins/nvim-bqf.lua
+++ lua/carlos/plugins/nvim-bqf.lua
@@ -12,9 +12,15 @@ return {
     },
   },
   {
+    -- This repo gets latest Vim plugin file from GitHub rather than the one included in the package manager.
     'junegunn/fzf',
-    build = function()
-      vim.fn['fzf#install']()
-    end,
+    --[[ 
+        The below install script is only needed if fzf is not installed in your system or if you want the latest from Github.
+        It first picks up the fzf binary available on the system. If it's not the latest version, it will install the latest from GitHub.
+        Most likely GitHub has a more recent version than the one included in the package (Homebrew), not urgent though
+    --]]
+    -- build = function()
+    --   vim.fn['fzf#install']()
+    -- end,
   },
 }
