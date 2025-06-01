160424d817bc66bc9d939f6e12ab28f1d87e640c
Carlos Calla <calla.alarcon@gmail.com>
2025-06-01 03:08:39 +0200

    nvim-bqf: lazy load fzf dependency

--- lua/carlos/plugins/nvim-bqf.lua
+++ lua/carlos/plugins/nvim-bqf.lua
@@ -4,23 +4,23 @@ return {
     ft = 'qf',
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter' },
+      {
+        -- This repo gets latest Vim plugin file from GitHub rather than the one included in the package manager.
+        'junegunn/fzf',
+        --[[ 
+        The below install script is only needed if fzf is not installed in your system or if you want the latest from Github.
+        It first picks up the fzf binary available on the system. If it's not the latest version, it will install the latest from GitHub.
+        Most likely GitHub has a more recent version than the one included in the package (Homebrew), not urgent though
+    --]]
+        -- build = function()
+        --   vim.fn['fzf#install']()
+        -- end,
+      },
     },
     opts = {
       preview = {
         win_height = 25,
       },
     },
-  },
-  {
-    -- This repo gets latest Vim plugin file from GitHub rather than the one included in the package manager.
-    'junegunn/fzf',
-    --[[ 
-        The below install script is only needed if fzf is not installed in your system or if you want the latest from Github.
-        It first picks up the fzf binary available on the system. If it's not the latest version, it will install the latest from GitHub.
-        Most likely GitHub has a more recent version than the one included in the package (Homebrew), not urgent though
-    --]]
-    -- build = function()
-    --   vim.fn['fzf#install']()
-    -- end,
   },
 }
