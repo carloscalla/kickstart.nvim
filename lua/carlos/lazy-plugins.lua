5254197af9ba6786158dfdb14f12170804b86bea
Carlos Calla <calla.alarcon@gmail.com>
2025-06-02 00:05:36 +0200

    debug: move to extras folder comments cleanup update lock file

--- lua/carlos/lazy-plugins.lua
+++ lua/carlos/lazy-plugins.lua
@@ -3,20 +3,16 @@
 -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
 require('lazy').setup({
 
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
+  -- Uncomment any of the lines below to enable them (you will need to restart nvim).
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/carlos/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
 
   { import = 'carlos.plugins' },
   -- { import = 'juanchi.plugins' },
 
-  { import = 'extras.plugins.copilot' },
-  { import = 'extras.plugins.avante' },
-
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  require 'extras.plugins.copilot',
+  require 'extras.plugins.avante',
+  -- require 'extras.plugins.debug',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
