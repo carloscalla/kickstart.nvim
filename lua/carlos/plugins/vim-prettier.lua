bf8d875768a6938eb55cf1a06ab3ff3506640b27
Carlos Calla <calla.alarcon@gmail.com>
2025-06-01 03:17:07 +0200

    vim-prettier: deprecate plugin

--- lua/carlos/plugins/vim-prettier.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'prettier/vim-prettier',
-  enabled = false,
-  build = 'yarn install --frozen-lockfile --production',
-  keys = {
-    { '<leader>pp', '<Plug>(PrettierAsync)', desc = 'Prettier: Format file' },
-  },
-}
