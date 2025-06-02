544bb95a9129cf3c6fb7bf936251f8efc515d874
Carlos Calla <calla.alarcon@gmail.com>
2025-06-02 18:14:24 +0200

    neotest: remove unneeded jest config, leaving default

--- lua/carlos/plugins/test.lua
+++ lua/carlos/plugins/test.lua
@@ -12,14 +12,7 @@ return {
     config = function()
       require('neotest').setup {
         adapters = {
-          require 'neotest-jest' {
-            jestCommand = 'npm test --',
-            jestConfigFile = 'custom.jest.config.ts',
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          },
+          require 'neotest-jest',
           require 'neotest-vitest',
         },
       }
