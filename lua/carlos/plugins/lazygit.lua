bec83176d6bd4a4903d6c9a1c866c7c7912cc269
Carlos Calla <calla.alarcon@gmail.com>
2025-06-05 23:56:41 +0200

    lazygit: mapping <esc><esc> to q for exiting

--- lua/carlos/plugins/lazygit.lua
+++ lua/carlos/plugins/lazygit.lua
@@ -1,5 +1,6 @@
 return {
   'kdheepak/lazygit.nvim',
+  lazy = true,
   cmd = {
     'LazyGit',
     'LazyGitConfig',
@@ -13,4 +14,12 @@ return {
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'lazygit' },
+      callback = function()
+        vim.keymap.set('t', '<Esc><Esc>', 'q', { buffer = 0, desc = 'Exit LazyGit' })
+      end,
+    })
+  end,
 }
