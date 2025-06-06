c8302dca54822be1d7108d2e0a697cc3269cb0bd
Carlos Calla <calla.alarcon@gmail.com>
2025-06-06 13:10:08 +0200

    terminal: fix lazygit <Esc>

--- lua/carlos/plugins/lazygit.lua
+++ lua/carlos/plugins/lazygit.lua
@@ -14,4 +14,14 @@ return {
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'lazygit' },
+      callback = function()
+        -- Fix <Esc>
+        -- Reset the terminal mode keymap <Esc><Esc> to exit terminal mode in keymaps.lua
+        vim.keymap.set('t', '<Esc>', '<Esc>', { buffer = 0, nowait = true })
+      end,
+    })
+  end,
 }
