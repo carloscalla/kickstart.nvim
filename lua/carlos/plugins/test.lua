4888a254971b6e14266fca69e579e698939d7182
Carlos Calla <calla.alarcon@gmail.com>
2025-06-12 16:58:09 +0200

    neotest: improve keymap descriptions

--- lua/carlos/plugins/test.lua
+++ lua/carlos/plugins/test.lua
@@ -19,21 +19,21 @@ return {
     end,
     keys = {
       -- stylua: ignore start
-      { '<leader>tf', function() require('neotest').run.run(vim.fn.expand '%') end,      desc = 'Run File' },
-      { '<leader>tr', function() require('neotest').run.run() end,                       desc = 'Run Nearest' },
-      { '<leader>tl', function() require('neotest').run.run_last() end,                  desc = 'Run Last' },
-      { '<leader>ts', function() require('neotest').summary.toggle() end,                desc = 'Toggle Summary' },
-      { '<leader>tO', function() require('neotest').output_panel.toggle() end,           desc = 'Toggle Output Panel' },
-      { '<leader>tS', function() require('neotest').run.stop() end,                      desc = 'Stop' },
-      { '<leader>tw', function() require('neotest').watch.toggle() end,                  desc = 'Toggle Watch' },
-      { '<leader>tW', function() require('neotest').watch.toggle(vim.fn.expand '%') end, desc = 'Toggle Watch File' },
+      { '<leader>tf', function() require('neotest').run.run(vim.fn.expand '%') end,      desc = 'Test: Run File' },
+      { '<leader>tr', function() require('neotest').run.run() end,                       desc = 'Test: Run Nearest' },
+      { '<leader>tl', function() require('neotest').run.run_last() end,                  desc = 'Test: Run Last' },
+      { '<leader>ts', function() require('neotest').summary.toggle() end,                desc = 'Test: Toggle Summary' },
+      { '<leader>tO', function() require('neotest').output_panel.toggle() end,           desc = 'Test: Toggle Output Panel' },
+      { '<leader>tS', function() require('neotest').run.stop() end,                      desc = 'Test: Stop' },
+      { '<leader>tw', function() require('neotest').watch.toggle() end,                  desc = 'Test: Toggle Watch' },
+      { '<leader>tW', function() require('neotest').watch.toggle(vim.fn.expand '%') end, desc = 'Test: Toggle Watch File' },
       -- stylua: ignore end
       {
         '<leader>to',
         function()
           require('neotest').output.open { enter = true, auto_close = true }
         end,
-        desc = 'Show Output',
+        desc = 'Test: Show Output',
       },
     },
   },
