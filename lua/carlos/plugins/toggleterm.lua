53b1b151466e6aaa7e56c78bbb967dc3d208b249
Carlos Calla <calla.alarcon@gmail.com>
2025-06-13 16:01:56 +0200

    toggleterm: add gh dash mapping
    me mame

--- lua/carlos/plugins/toggleterm.lua
+++ lua/carlos/plugins/toggleterm.lua
@@ -6,10 +6,33 @@ return {
         size = 30,
         direction = 'horizontal',
       }
+
+      local Terminal = require('toggleterm.terminal').Terminal
+      local gh_dash = Terminal:new {
+        cmd = 'gh dash',
+        dir = 'git_dir',
+        direction = 'float',
+        on_open = function(term)
+          vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<Esc>', '<Esc>', { nowait = true })
+        end,
+      }
+
+      _G.Toggleterm = _G.Toggleterm or {}
+
+      function _G.Toggleterm._gh_dash_toggle()
+        gh_dash:toggle()
+      end
     end,
     keys = {
       { '<leader>tt', '<Cmd>ToggleTerm<CR>', desc = 'Toggle Terminal' },
       { '<leader>tn', '<Cmd>TermNew<CR>', desc = 'New Terminal' },
+      {
+        '<leader>tG',
+        function()
+          _G.Toggleterm._gh_dash_toggle()
+        end,
+        desc = 'Toggle gh dash',
+      },
     },
   },
 }
