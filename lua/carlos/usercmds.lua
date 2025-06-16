224c82fdc521e49463cc9ca73bd737250b165443
Carlos Calla <calla.alarcon@gmail.com>
2025-06-16 18:38:24 +0200

    usercmds: add cmd for nearest package.json path

--- lua/carlos/usercmds.lua
+++ lua/carlos/usercmds.lua
@@ -15,3 +15,17 @@ vim.api.nvim_create_user_command('FileName', function()
   vim.fn.setreg('+', fileName)
   print(fileName)
 end, { desc = 'Copy file name of current buffer to clipboard' })
+
+vim.api.nvim_create_user_command('NearestPackagePath', function()
+  local Path = require 'plenary.path'
+  local current_file_path = Path:new(vim.fn.expand '%:p')
+  local nearest_package_json = current_file_path:find_upwards 'package.json'
+
+  if nearest_package_json and nearest_package_json:exists() then
+    local package_dir = vim.fn.fnamemodify(nearest_package_json:absolute(), ':h:p')
+    vim.fn.setreg('+', package_dir)
+    print(package_dir)
+  else
+    vim.notify(string.format "Can't find a parent package.json", vim.log.levels.ERROR, { title = "Couldn't find ancestor package.json" })
+  end
+end, { desc = 'Copy file name of current buffer to clipboard' })
