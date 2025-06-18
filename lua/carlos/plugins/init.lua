94ae2a646b2ebe25a7734fe12255688fc27658e7
Carlos Calla <calla.alarcon@gmail.com>
2025-06-18 02:36:45 +0200

    vim-tmux-navigator: add plugin for seamless navigation between tmux and neobim

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -68,4 +68,22 @@ return {
       vim.cmd [[ let g:previm_open_cmd = 'open -a Google\ Chrome']]
     end,
   },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
 }
