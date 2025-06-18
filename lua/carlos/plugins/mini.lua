91816f24b02a05fc97d76e7622c1d34bcf72ee1c
Carlos Calla <calla.alarcon@gmail.com>
2025-06-18 02:27:48 +0200

    mini: add rename callback for mini files

--- lua/carlos/plugins/mini.lua
+++ lua/carlos/plugins/mini.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniFilesActionRename',
+  callback = function(event)
+    Snacks.rename.on_rename_file(event.data.from, event.data.to)
+  end,
+})
+
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   lazy = false,
