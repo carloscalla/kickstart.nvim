367c9b188abd10b059eb6826197bb77b7a7970f6
Carlos Calla <calla.alarcon@gmail.com>
2025-06-26 12:33:34 +0200

    nvim-ts-autotag: update lazy load events

--- lua/carlos/plugins/nvim-ts-autotag.lua
+++ lua/carlos/plugins/nvim-ts-autotag.lua
@@ -1,6 +1,6 @@
 return {
   'windwp/nvim-ts-autotag',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = { 'BufReadPost', 'BufNewFile', 'BufWritePost' },
   config = function()
     require('nvim-ts-autotag').setup {
       opts = {
