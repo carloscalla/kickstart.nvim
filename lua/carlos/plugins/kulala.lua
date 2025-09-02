c3e352472c8e0b853bddf920194ff4dbea49e39a
Carlos Calla <calla.alarcon@gmail.com>
2025-09-02 15:31:00 +0200

    kulala: use default keymaps

--- lua/carlos/plugins/kulala.lua
+++ lua/carlos/plugins/kulala.lua
@@ -11,13 +11,18 @@ return {
     ft = 'http',
     keys = {
       { '<leader>R', '', desc = '+Rest', ft = 'http' },
-      { '<leader>Rs', "<cmd>lua require('kulala').run()<cr>", desc = 'Send the request', ft = 'http' },
-      { '<leader>Rt', "<cmd>lua require('kulala').toggle_view()<cr>", desc = 'Toggle headers/body', ft = 'http' },
-      { '<leader>Rp', "<cmd>lua require('kulala').jump_prev()<cr>", desc = 'Jump to previous request', ft = 'http' },
-      { '<leader>Rn', "<cmd>lua require('kulala').jump_next()<cr>", desc = 'Jump to next request', ft = 'http' },
-      { '<leader>Ri', "<cmd>lua require('kulala').inspect()<cr>", desc = 'Inspect request', ft = 'http' },
+      -- { '<leader>Rs', "<cmd>lua require('kulala').run()<cr>", desc = 'Send the request', ft = 'http' },
+      -- { '<leader>Rt', "<cmd>lua require('kulala').toggle_view()<cr>", desc = 'Toggle headers/body', ft = 'http' },
+      -- { '<leader>Rp', "<cmd>lua require('kulala').jump_prev()<cr>", desc = 'Jump to previous request', ft = 'http' },
+      -- { '<leader>Rn', "<cmd>lua require('kulala').jump_next()<cr>", desc = 'Jump to next request', ft = 'http' },
+      -- { '<leader>Ri', "<cmd>lua require('kulala').inspect()<cr>", desc = 'Inspect request', ft = 'http' },
     },
-    opts = {},
+    -- use opts = {} when uncommenting the keymaps above
+    opts = {
+      global_keymaps = true,
+      global_keymaps_prefix = '<leader>R',
+      kulala_keymaps_prefix = '',
+    },
   },
   {
     'nvim-treesitter/nvim-treesitter',
