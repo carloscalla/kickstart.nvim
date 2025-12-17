22600a1011d3b52b070791675ed8641736e0ffdf
Carlos Calla <calla.alarcon@gmail.com>
2025-12-17 12:11:32 +0100

    flash: update treesitter mapping to work on tmux

--- lua/carlos/plugins/flash.lua
+++ lua/carlos/plugins/flash.lua
@@ -15,7 +15,9 @@ return {
   keys = {
     -- stylua: ignore start
     { "s"    , mode = { "n", "x" },      function() require("flash").jump() end,              desc = "Flash" },
-    { "<C-,>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    -- The following keymap doesn't work in tmux :(
+    -- { "<C-,>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "<leader>ss", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,   desc = "Flash Treesitter" },
     { "r"    , mode = { "o" },           function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R"    , mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
