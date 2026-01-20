68050f84ed800c8e389528c4045525cbf50ba223
Carlos Calla <calla.alarcon@gmail.com>
2026-01-20 21:06:41 +0100

    gh.nvim: add plugin to try out

--- lua/carlos/plugins/init.lua
+++ lua/carlos/plugins/init.lua
@@ -113,4 +113,21 @@ return {
       split_size = 0.3,
     },
   },
+
+  {
+    'ldelossa/gh.nvim',
+    dependencies = {
+      {
+        'ldelossa/litee.nvim',
+        config = function()
+          require('litee.lib').setup()
+        end,
+      },
+    },
+    config = function()
+      require('litee.gh').setup {
+        icon_set = 'codicons',
+      }
+    end,
+  },
 }
