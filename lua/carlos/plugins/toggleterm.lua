9ca5a4e5ebb67a321df2df198321fb569ab552c0
Carlos Calla <calla.alarcon@gmail.com>
2026-01-27 12:45:22 +0100

    flatten: moved to extras

--- lua/carlos/plugins/toggleterm.lua
+++ lua/carlos/plugins/toggleterm.lua
@@ -78,38 +78,4 @@ return {
       -- },
     },
   },
-  {
-    -- Open files and command output from wezterm, kitty, and neovim terminals in
-    -- your current neovim instance
-    'willothy/flatten.nvim',
-    -- Ensure that it runs first to minimize delay when opening file from terminal
-    lazy = false,
-    priority = 1001,
-    enabled = false, -- Disabling because it breaks neotest :(
-    opts = function()
-      ---@type Terminal?
-      local saved_terminal
-
-      return {
-        window = {
-          open = 'alternate',
-        },
-        hooks = {
-          pre_open = function()
-            local term = require 'toggleterm.terminal'
-            local termid = term.get_focused_id()
-            saved_terminal = term.get(termid)
-          end,
-          post_open = function(bufnr, winnr, ft, is_blocking)
-            if saved_terminal and vim.bo[saved_terminal.bufnr].filetype == 'lazygit' then
-              saved_terminal:shutdown()
-            else
-              -- If it's a normal file, just switch to its window
-              vim.api.nvim_set_current_win(winnr)
-            end
-          end,
-        },
-      }
-    end,
-  },
 }
